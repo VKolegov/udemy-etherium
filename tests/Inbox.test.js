d52130eed7dad86ab3ce6d82d39afb0d337335c5
Vladislav Kolegov <dev@vkolegov.ru>
2024-05-17 08:45:21 +0700

    basic functionality tests (ep 54)

--- tests/Inbox.test.js
+++ tests/Inbox.test.js
@@ -5,6 +5,8 @@ const {interface, bytecode} = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
+const initialMessage = 'hey nigga';
+
 let accounts;
 let inbox;
 
@@ -12,13 +14,30 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     inbox = await (new web3.eth.Contract(JSON.parse(interface)))
-        .deploy({data: bytecode, arguments: ['Hi nigga']})
+        .deploy({data: bytecode, arguments: [ initialMessage ]})
         .send({from: accounts[0], gas: '1000000'});
 });
 
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+        console.log(inbox.options.address);
+    })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, initialMessage);
+    });
+
+    it('can change message', async () => {
+        const newMessage = 'bye nigga';
+        const receipt = await inbox.methods.setMessage(newMessage).send({from: accounts[0]});
+        console.log(receipt);
+
+        const currentMessage = await inbox.methods.message().call();
+        console.log(currentMessage);
+
+        assert.strictEqual(currentMessage, newMessage);
     })
 });
