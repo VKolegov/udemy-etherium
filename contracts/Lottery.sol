69727b9a36987bd32bae98d15201af80cd09325a
Vladislav Kolegov <dev@vkolegov.ru>
2024-05-28 12:52:33 +0700

    basic lottery contract (ep 81)

---
+++ contracts/Lottery.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4;
+
+contract Lottery {
+    uint minGwei;
+    address public manager;
+    address[] public players; 
+
+
+    constructor(uint _minGwei) public  {
+        manager = msg.sender;
+        minGwei = _minGwei * 1000000000;
+    }
+
+    function enter() public payable {
+        require(msg.value >= minGwei);
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(
+            abi.encode(block.difficulty, now, players)
+        ));
+    }
+
+    function pickWinner() public {
+
+        require(msg.sender == manager);
+
+        uint index = random() % players.length;
+        address winner = address(players[index]);
+        winner.transfer(address(this).balance);
+
+        players = new address[](0);
+    }
+}
