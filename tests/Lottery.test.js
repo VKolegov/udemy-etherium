3300bce635af34a2e64b7b4adc052c37a48057bc
Vladislav Kolegov <dev@vkolegov.ru>
2024-06-01 17:08:31 +0700

    test multiple accounts enter (ep 91)

---
+++ tests/Lottery.test.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const {interface, bytecode} = require('../compile');
+
+const web3 = new Web3(ganache.provider());
+
+const minGweiToEnter = 5;
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await (new web3.eth.Contract(JSON.parse(interface)))
+        .deploy({data: bytecode, arguments: [minGweiToEnter]})
+        .send({from: accounts[0], gas: '1000000'});
+});
+
+
+describe('Lottery', () => {
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address);
+        console.log(lottery.options.address);
+    });
+
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.strictEqual(accounts[0], players[0]);
+        assert.strictEqual(1, players.length);
+    });
+
+    it('allows multiple accounts to enter', async () => {
+
+        const testAccounts = accounts.slice(3, 6);
+
+        for (let account of testAccounts) {
+            await lottery.methods.enter().send({
+                from: account,
+                value: web3.utils.toWei(String(0.02), 'ether'),
+            });
+        }
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.strictEqual(testAccounts.length, players.length);
+        for (let player of players) {
+            assert.strictEqual(true, testAccounts.includes(player));
+        }
+    });
+});
